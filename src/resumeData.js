d1b9933bb63a0c69dbc70cc343ee6796e701361e
qcode1 <nii.quartey19@gmail.com>
2022-10-25 15:22:02 +0000

    Initial Commit

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://qcode1.github.io/",
+    "name": "Nii Quateboye Quartey",
+    "role": "Software Engineer / Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com/in/niiquateboyequartey/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I have a keen interest in developing web solutions for clients with industry experience in various parts of frontend & backend development as well as REST APIs. I quickly adapt to new technologies, can work remotely and would love to collaborate on exciting projects.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/niiquateboyequartey/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/qcode1",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a Junior Software Developer with INNOBIZ ICT Solutions Limited where I develop and maintain web solutions for Ghanaian missions around the world to digitize Consular management process and promote their online presence. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Accra, Ghana",
+    "website":"https://qcode1.github.io",
+    "education":[
+      {
+        "UniversityName":"Ashesi University College",
+        "specialization":"BSC. Computer SCience",
+        "MonthOfPassing":"Dec",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"INNOBIZ ICT Solutions Limited",
+        "specialization":"Junior Software Developer",
+        "MonthOfLeaving":"Current",
+        "YearOfLeaving":"",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Nsano Limited",
+        "specialization":"Software Engineer",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"Python"
+      },
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
