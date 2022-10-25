f3445a29caa1d3a0fcbb8132bb9418e768206339
qcode1 <nii.quartey19@gmail.com>
2022-10-25 15:35:57 +0000

    Updates

---
+++ precache-manifest.a4c68725fa3ffee5c34904e755459984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c3232d70950d7c694e0b",
+    "url": "/static/css/main.0abc759b.chunk.css"
+  },
+  {
+    "revision": "c3232d70950d7c694e0b",
+    "url": "/static/js/main.c3232d70.chunk.js"
+  },
+  {
+    "revision": "9b0a642ea054f275e8c9",
+    "url": "/static/js/1.9b0a642e.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "27dd1466f55fae718cf8cf5d56986bbb",
+    "url": "/index.html"
+  }
+];
